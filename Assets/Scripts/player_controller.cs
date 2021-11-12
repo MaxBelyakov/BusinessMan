963071e08839296bc811a2ba2129ea1e6d1ab384
MaxBelyakov <belyakovdevelopment@gmail.com>
2021-11-12 19:06:10 +0300

    v.1.1

---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class player_controller : MonoBehaviour {
+
+    public float movespeed = 3;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update () {
+
+        /* Moving up and down */
+		if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movespeed * Time.deltaTime, 0f, 0f));
+        }
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * movespeed * Time.deltaTime, 0f));
+        }
+
+        /* Transfer parametrs to Animator */
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+    }
+}
