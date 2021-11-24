f266bc0206818a4d9f002519a1a7cca43358c787
MaxBelyakov <belyakovdevelopment@gmail.com>
2021-11-24 14:50:30 +0300

    v.1.9

--- Assets/Scripts/FreePlaceEnter.cs
+++ Assets/Scripts/FreePlaceEnter.cs
@@ -4,33 +4,66 @@ public class FreePlaceEnter : MonoBehaviour {
 
     public GameObject textField; //Connected to Text Prefab
     public GameObject building; //Connected to building Prefab
+    public GameObject manager; //Connected to manager Prefab
     private bool build_request = false;
+    private bool buy_manager_request = false;
+    private bool buy_truck_request = false;
     private GameObject text;
 
     private void Update() {
-        /* Create new building insead free place */
-        if (Input.GetButtonDown("Fire1") && build_request)
+        /* Waiting for money request */
+        if (Input.GetButtonDown("Fire1"))
         {
-            var new_building = Instantiate(building, gameObject.transform.position, Quaternion.Euler(Vector3.zero));
-            new_building.transform.SetParent(GameObject.Find("Buildings").transform);
-            Destroy(gameObject);
+            /* Create new building */
+            if (build_request)
+            {
+                var new_building = Instantiate(building, gameObject.transform.position, Quaternion.Euler(Vector3.zero));
+                new_building.transform.SetParent(GameObject.Find("Buildings").transform);
+                new_building.transform.tag = "Buildings";
+                Destroy(gameObject);
+            }
+            /* Hire manager */
+            if (buy_manager_request)
+            {
+                /* Searching free tables */
+                foreach (GameObject child in GameObject.FindGameObjectsWithTag("WorkTable"))
+                {
+                    if (child.transform.childCount == 0)
+                    {
+                        var manager_position = new Vector3(child.transform.position.x, child.transform.position.y + 0.3f, child.transform.position.z);
+                        var new_manager = Instantiate(manager, manager_position, Quaternion.Euler(Vector3.zero));
+                        new_manager.transform.SetParent(child.transform);
+                        break;
+                    }
+                }
+            }
         }
     }
 
     /* Show text */
     private void OnTriggerEnter2D(Collider2D collision) {
-        var text_position = new Vector3(collision.transform.position.x + 1.7f, collision.transform.position.y + 0.7f, collision.transform.position.z);
-        text = Instantiate(textField, text_position, Quaternion.Euler(Vector3.zero));
+        if (collision.name == "player_0")
+        {
+            var text_position = new Vector3(collision.transform.position.x + 1.7f, collision.transform.position.y + 0.7f, collision.transform.position.z);
+            text = Instantiate(textField, text_position, Quaternion.Euler(Vector3.zero));
 
-        text.GetComponent<TextController>().SelectText("buy", gameObject.name);
-        text.transform.SetParent(collision.transform);
+            text.GetComponent<TextController>().SelectText("buy", gameObject.name);
+            text.transform.SetParent(collision.transform);
 
-        build_request = true;
+            if (gameObject.name == "buyManagerCell")
+                buy_manager_request = true;
+            else if (gameObject.name == "buyTruckCell")
+                buy_truck_request = true;
+            else
+                build_request = true;
+        } 
     }
 
     /* Hide text */
     private void OnTriggerExit2D(Collider2D collision) {
         Destroy(text);
         build_request = false;
+        buy_truck_request = false;
+        buy_manager_request = false;
     }
 }
--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -21,6 +21,14 @@ public class TextController : MonoBehaviour {
             {
                 displayText.text = "Blacksmith" + "\n" + "Resourses: 150.000" + "\n" + "Build price: 7.000" + "\n" + "Press 'space' to build";
             }
+            else if (name == "buyManagerCell")
+            {
+                displayText.text = "Hire new manager" + "\n" + "Costs: 10.000 per month" + "\n" + "Productivity: control 3 trucks" + "\n" + "Press 'space' to hire";
+            }
+            else if (name == "buyTruckCell")
+            {
+                displayText.text = "Buy new truck" + "\n" + "Price: 50.000" + "\n" + "Press 'space' to buy";
+            }
         }
     }
 }
